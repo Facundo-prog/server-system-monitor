e8c69f3178731decbb3ef6bc05872ec1b1b2e50f
facundo.prog <facundo.prog@gmail.com>
2023-08-15 12:39:33 -0300

    update

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: "./dist",
+    outDir: "../backend/static",
     assetsDir: "static/assets/",
+    emptyOutDir: true,
   },
   plugins: [react()],
 })
